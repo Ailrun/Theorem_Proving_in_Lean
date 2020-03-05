1f86aeb2bf4bc9e10ab0568847e6158c21d5904b
Junyoung Clare Jang <jjc9310@gmail.com>
2020-03-04 20:52:14 -0500

    Start Ch6

---
+++ src/ch6/ex0201.lean
@@ -0,0 +1,17 @@
+section
+  variables x y : ℕ
+
+  def double := x + x
+
+  #check double y
+  #check double (2 * x)
+
+  theorem t1 : double (x + y) = double x + double y :=
+    by simp [double]
+
+  #check t1 y
+  #check t1 (2 * x)
+
+  theorem t2 : double (x * y) = double x * y :=
+    by simp [double, add_mul]
+end
